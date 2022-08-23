65e10064b62213a22010e9ffd53480bc2ecaaaaa
Maicon Ramos <contato@maiconramos.com.br>
2022-08-23 17:47:16 -0300

    Change style title

--- frontend/src/components/Title/index.js
+++ frontend/src/components/Title/index.js
@@ -3,8 +3,8 @@ import Typography from "@mui/material/Typography";
 
 export default function Title(props) {
 	return (
-		<Typography variant="h5" color="primary" gutterBottom>
-			{props.children}
+		<Typography variant="h5" color="dark" gutterBottom>
+			<strong>{props.children}</strong>
 		</Typography>
 	);
 }
